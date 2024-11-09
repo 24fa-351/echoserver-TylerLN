edb049a6fe17f515f2e3bc4b15bfc8da2f9c4035
root <root@CSUF-1629312566.localdomain>
2024-11-08 18:27:28 -0800

    Fixed and added thread

--- echo.c
+++ echo.c
@@ -7,101 +7,109 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define DEFAULT_PORT 46645
 #define LISTEN_BACKLOG 5
+#define MAX_BUFFER_SIZE 1024
+#define DEFAULT_PORT 46645
 
-void* handle_connection(void* a_client_ptr) {
-  int a_client = *(int*)a_client_ptr;
-  free(a_client_ptr);
+void handle_connection(int* sock_fd_ptr) {
+  int sock_fd = *sock_fd_ptr;
+  free(sock_fd_ptr);
 
-  char buffer[1024];
+  printf("Handling connection on %d\n", sock_fd);
+  char buffer[MAX_BUFFER_SIZE];
   int bytes_read;
 
   while (1) {
-    bytes_read = read(a_client, buffer, sizeof(buffer) - 1);
-    if (bytes_read == 0) {
-      printf("End of connection on socket_fd %d\n", a_client);
-      break;
-    }
-    if (bytes_read < 0) {
-      perror("Error reading from socket");
-      break;
-    }
+    bytes_read = read(sock_fd, buffer, sizeof(buffer));
+    if (bytes_read > 0) {
+      buffer[bytes_read] = '\0';
+      printf("Received From Connection %d: %s\n", sock_fd, buffer);
 
-    buffer[bytes_read] = '\0';
-    printf("Received from socket_fd %d: %s\n", a_client, buffer);
-    write(a_client, buffer, bytes_read);
+      if (strlen(buffer) == 0) {
+        printf("Closing Connection on %d\n", sock_fd);
+        break;
+      }
+
+      if (write(sock_fd, buffer, bytes_read) == -1) {
+        perror("Write failed");
+        break;
+      }
+    } else if (bytes_read == 0) {
+      printf("Closing Connection on %d\n", sock_fd);
+      break;
+    } else {
+      perror("Read failed");
+      break;
+    }
   }
 
-  close(a_client);
-  return NULL;
+  printf("Closing connection on %d\n", sock_fd);
+  close(sock_fd);
 }
 
 int main(int argc, char* argv[]) {
   int port = DEFAULT_PORT;
 
   if (argc > 1) {
-    if (strcmp(argv[1], "-p") == 0 && argc > 2) {
-      port = atoi(argv[2]);
-      if (port <= 0 || port > 65535) {
-        fprintf(stderr, "Invalid port number: %s\n", argv[2]);
-        return 1;
-      }
-    } else {
-      fprintf(stderr, "Usage: %s -p port\n", argv[0]);
+    port = atoi(argv[1]);
+    if (port <= 0 || port > 65535) {
+      fprintf(
+          stderr,
+          "Invalid port number. Please provide a port between 1 and 65535.\n");
       return 1;
     }
   }
 
+  printf("Server Listening on Port: %d\n", port);
+
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (socket_fd < 0) {
-    perror("Socket creation failed");
+  if (socket_fd == -1) {
+    perror("Failed to create socket");
     return 1;
   }
 
-  struct sockaddr_in socket_address;
-  memset(&socket_address, 0, sizeof(socket_address));
-  socket_address.sin_family = AF_INET;
-  socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
-  socket_address.sin_port = htons(port);
+  struct sockaddr_in server_address;
+  memset(&server_address, '\0', sizeof(server_address));
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_address.sin_port = htons(port);
 
-  if (bind(socket_fd, (struct sockaddr*)&socket_address,
-           sizeof(socket_address)) < 0) {
-    perror("Bind failed");
-    close(socket_fd);
+  int return_val = bind(socket_fd, (struct sockaddr*)&server_address,
+                        sizeof(server_address));
+  if (return_val == -1) {
+    perror("Failed to bind socket");
     return 1;
   }
 
-  if (listen(socket_fd, LISTEN_BACKLOG) < 0) {
-    perror("Listen failed");
-    close(socket_fd);
+  return_val = listen(socket_fd, LISTEN_BACKLOG);
+  if (return_val == -1) {
+    perror("Failed to listen on socket");
     return 1;
   }
-
-  printf("Server listening on port %d\n", port);
 
   struct sockaddr_in client_address;
   socklen_t client_address_len = sizeof(client_address);
 
   while (1) {
-    int client_fd = accept(socket_fd, (struct sockaddr*)&client_address,
-                           &client_address_len);
-    if (client_fd < 0) {
-      perror("Accept failed");
-      continue;
-    }
-
-    int* client_fd_ptr = malloc(sizeof(int));
-    *client_fd_ptr = client_fd;
-
     pthread_t thread;
-    if (pthread_create(&thread, NULL, handle_connection, client_fd_ptr) != 0) {
-      perror("Could not create thread");
-      free(client_fd_ptr);
-      close(client_fd);
+    int* client_fd_buf = malloc(sizeof(int));
+    if (!client_fd_buf) {
+      perror("Failed to allocate memory for client file descriptor");
       continue;
     }
 
+    *client_fd_buf = accept(socket_fd, (struct sockaddr*)&client_address,
+                            &client_address_len);
+    if (*client_fd_buf == -1) {
+      perror("Failed to accept connection");
+      free(client_fd_buf);
+      continue;
+    }
+
+    printf("Accepted connection on %d\n", *client_fd_buf);
+
+    pthread_create(&thread, NULL, (void* (*)(void*))handle_connection,
+                   (void*)client_fd_buf);
     pthread_detach(thread);
   }
 
