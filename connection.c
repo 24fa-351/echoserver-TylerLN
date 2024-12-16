b7ac6ffcb5d25e87663051e7ef50277905fa9b92
TylerLN <tylerlngu@csu.fullerton.edu>
2024-12-15 17:32:08 -0800

    Format

--- connection.c
+++ connection.c
@@ -6,38 +6,38 @@
 
 #define MAX_BUFFER_SIZE 1024
 
-void handle_connection(int* sock_fd_ptr) {
-  int sock_fd = *sock_fd_ptr;
-  free(sock_fd_ptr);
+void handle_connection(int *sock_fd_ptr) {
+    int sock_fd = *sock_fd_ptr;
+    free(sock_fd_ptr);
 
-  printf("Handling connection on %d\n", sock_fd);
-  char buffer[MAX_BUFFER_SIZE];
-  int bytes_read;
+    printf("Handling connection on %d\n", sock_fd);
+    char buffer[MAX_BUFFER_SIZE];
+    int bytes_read;
 
-  while (1) {
-    bytes_read = read(sock_fd, buffer, sizeof(buffer));
-    if (bytes_read > 0) {
-      buffer[bytes_read] = '\0';
-      printf("Received From Connection %d: %s\n", sock_fd, buffer);
+    while (1) {
+        bytes_read = read(sock_fd, buffer, sizeof(buffer));
+        if (bytes_read > 0) {
+            buffer[bytes_read] = '\0';
+            printf("Received From Connection %d: %s\n", sock_fd, buffer);
 
-      if (strlen(buffer) == 0) {
-        printf("Closing Connection on %d\n", sock_fd);
-        break;
-      }
+            if (strlen(buffer) == 0) {
+                printf("Closing Connection on %d\n", sock_fd);
+                break;
+            }
 
-      if (write(sock_fd, buffer, bytes_read) == -1) {
-        perror("Write failed");
-        break;
-      }
-    } else if (bytes_read == 0) {
-      printf("Closing Connection on %d\n", sock_fd);
-      break;
-    } else {
-      perror("Read failed");
-      break;
+            if (write(sock_fd, buffer, bytes_read) == -1) {
+                perror("Write failed");
+                break;
+            }
+        } else if (bytes_read == 0) {
+            printf("Closing Connection on %d\n", sock_fd);
+            break;
+        } else {
+            perror("Read failed");
+            break;
+        }
     }
-  }
 
-  printf("Closing connection on %d\n", sock_fd);
-  close(sock_fd);
+    printf("Closing connection on %d\n", sock_fd);
+    close(sock_fd);
 }
--- connection.h
+++ connection.h
@@ -1,6 +1,6 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
-void handle_connection(int* sock_fd_ptr);
+void handle_connection(int *sock_fd_ptr);
 
 #endif
